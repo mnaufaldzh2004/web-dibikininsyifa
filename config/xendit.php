61cbb6598c5c7a1d0a3ae38fe76708ee9ffd073c
Haqi25 <baihaqi1401@student.ub.ac.id>
2026-05-16 23:09:31 +0700

    fix: env secret key

--- config/xendit.php
+++ config/xendit.php
@@ -3,6 +3,6 @@
 return [
     'xendit' => [
         'api_key' => env('XENDIT_SECRET_KEY'),
-        'callback_token' => env('XENDIT_CALLBACK_TOKEN')
+        'callback_token' =>env('XENDIT_CALLBACK_TOKEN')
     ]
 ];
